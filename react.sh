9a0a9b3c9fef67102ad856247f375253f2c4e1eb
Hunan-tiger <1787169809@qq.com>
2024-01-06 19:23:45 +0800

    git commit

--- react.sh
+++ react.sh
@@ -26,9 +26,13 @@ cd /home_data/home/v-lijm/projects/react    ##工作目录
 ## python train.py --batch-size 4  --gpu-ids 0  -lr 0.00001  --kl-p 0.00001 -e 50  -j 8 --online  --window-size 16 --outdir results/train_online
 
 ## 离线任务评估
-python evaluate.py  --resume results/train_offline/best_checkpoint.pth  --gpu-ids 0  --outdir results/val_offline --split val
+## python evaluate.py  --resume results/train_offline/best_checkpoint.pth  --gpu-ids 0  --outdir results/val_offline --split val
 ## 在线任务评估
 ## python evaluate.py  --resume ./results/train_online/best_checkpoint.pth  --gpu-ids 0  --online --outdir results/val_online --split val
+
+## 计算FRRea
+python -m pytorch_fid  ./results/val_offline/val/fid/real  ./results/val_offline/val/fid/fake
+
 echo end on $(date)
 
 
